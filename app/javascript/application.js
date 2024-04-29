2d1ca9fd147cf08cd3b16860f4f77a0a4221bb5f
DeonBuble <Osetrov01v1@list.ru>
2024-04-30 00:15:32 +0500

    less9 fin1

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,9 @@
 // Entry point for the build script in your package.json
 import Rails from "@rails/ujs"
 import TurboLinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
 
 import '@popperjs/core'
 import 'bootstrap/js/dist/dropdown'
 
 Rails.start()
 TurboLinks.start()
-ActiveStorage.start()
